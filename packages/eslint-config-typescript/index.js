ac1d1ab11a4d07f87648e125c9e1714f8a6309e8
Truong Hoang Phuong <thphuong1093@gmail.com>
2022-02-21 16:00:44 +0700

    fix(ts): properly override ts rules

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -2,4 +2,30 @@ module.exports = {
     extends: [
         'airbnb-typescript/base'
     ],
+    rules: {
+        '@typescript-eslint/indent': ['error', 4, {
+            SwitchCase: 1,
+            VariableDeclarator: 1,
+            outerIIFEBody: 1,
+            FunctionDeclaration: {
+                parameters: 1,
+                body: 1
+            },
+            FunctionExpression: {
+                parameters: 1,
+                body: 1
+            },
+            CallExpression: {
+                arguments: 1
+            },
+            ArrayExpression: 1,
+            ObjectExpression: 1,
+            ImportDeclaration: 1,
+            flatTernaryExpressions: false,
+            ignoreComments: false
+        }],
+        '@typescript-eslint/no-use-before-define': ['error', {
+            functions: false,
+        }],
+    }
 }
