dc0303390f615a86707838008dcb1b2b37d308f4
Truong Hoang Phuong <thphuong1093@gmail.com>
2022-02-21 14:33:16 +0700

    Initial commit

---
+++ packages/eslint-config-nuxt/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        'plugin:nuxt/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    }
+};
---
+++ packages/eslint-config-vue/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    extends: [
+        'plugin:vue/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    rules: {
+        'vue/html-indent': ['error', 4],
+        'vue/script-indent': ['error', 4, {
+            'baseIndent': 1,
+            'switchCase': 1
+        }],
+        'vue/max-attributes-per-line': ['warn', {
+            'singleline': 3
+        }],
+        'vue/require-default-prop': 'off'
+    },
+};
